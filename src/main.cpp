6cde99284ad3fed75c4e7ab9ac7c8221ed86067f
adrian <brasi@mailfence.com>
2024-08-07 23:59:22 -0700

    ligma

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,117 @@
 #include <iostream>
+#include <string>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+GLFWwindow *window = nullptr;
+
+static unsigned int compileShader(unsigned int type, const std::string& source)
+{
+    unsigned int id = glCreateShader(type);
+    const char* src = source.c_str();
+    glShaderSource(id, 1, &src, nullptr);
+    glCompileShader(id);
+
+    int result;
+    glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+    if (result == GL_FALSE)
+    {
+        int length;
+        glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+        char* message = (char*)alloca(length * sizeof(char));
+        glGetShaderInfoLog(id, length, &length, message);
+        std::cout << "Failed to complile Shader" << std::endl;
+        std::cout << message << std::endl;
+        glDeleteShader(id);
+        return 0;
+    }
+
+    return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader)
+{
+    unsigned int program = glCreateProgram();
+    unsigned int vs = compileShader(GL_VERTEX_SHADER, vertexShader);
+    unsigned int fs = compileShader(GL_FRAGMENT_SHADER, fragmentShader);
+
+    glAttachShader(program, vs);
+    glAttachShader(program, fs);
+    glLinkProgram(program);
+    glValidateProgram(program);
+
+    glDeleteShader(vs);
+    glDeleteShader(fs);
+
+    return program;
+}
+
+void init()
+{
+    if (!glfwInit())
+    {
+        std::cerr << "GLFW IS FUCKED" << std::endl;
+    }
+    window = glfwCreateWindow(800, 800, "A FUCKING TRIANGLE", NULL, NULL);
+    if (!window)
+    {
+        std::cerr << "WINDOW IS FUCKED" << std::endl;
+        glfwTerminate();
+    }
+    glfwMakeContextCurrent(window);
+    if (glewInit() != GLEW_OK)
+    {
+        std::cerr << "GLEW IS FUCKED" << std::endl;
+        glfwTerminate();
+    }
+}
+
+void cleanup()
+{
+    window = nullptr;
+    glfwTerminate();
+}
 
 int main()
 {
-    GLFWwindow* window;
 
+    init();
 
-    if (!glfwInit())
-        return -1;
-
-
-    window = glfwCreateWindow(1280, 720, "Hello World", NULL, NULL);
-    if (!window)
+    float positions[6] =
     {
-        glfwTerminate();
-        return -1;
-    }
-
-    glfwMakeContextCurrent(window);
-
-    if (glewInit() != GLEW_OK) {
-        std::cerr << "GLEW IS FUCKED" << std::endl;
-    }
-
-    float positions[6] = {
         -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+        0.0f, 0.5f,
+        0.5f, -0.5f
     };
-
 
     unsigned int buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
+    std::string vertexShader =
+        "#version 330 core\n"
+        "layout(location = 0) in vec4 position;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   gl_Position = position;\n"
+        "}\n";
+
+    std::string fragmentShader =
+        "#version 330 core\n"
+        "layout(location = 0) out vec4 color;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "   color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "}\n";
+
+    unsigned int shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
 
     while (!glfwWindowShouldClose(window))
     {
@@ -49,6 +123,8 @@ int main()
         glfwPollEvents();
     }
 
-    glfwTerminate();
+    glDeleteProgram(shader);
+
+    cleanup();
     return 0;
 }
